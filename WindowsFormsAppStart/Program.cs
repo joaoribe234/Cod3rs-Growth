bb937bafa930c1f36f8ab909dd66bc2b5c552aac
João Vitor Ribeiro Gonçalves <joaoribe234@gmail.com>
2023-05-04 14:07:35 -0300

    implementação migracao

--- WindowsFormsAppStart/Program.cs
+++ WindowsFormsAppStart/Program.cs
@@ -1,22 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace WindowsFormsAppStart
 {
     internal static class Program
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
+        private static string conectionString = ConfigurationManager.ConnectionStrings["BancoClientes"].ConnectionString;
+
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new TelaInicial());
+
+            using (var serviceProvider = CreateServices())
+            using (var scope = serviceProvider.CreateScope())
+            {
+
+                UpdateDatabase(scope.ServiceProvider);
+            }
+
+        }
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
+        }
+
+        private static ServiceProvider CreateServices()
+            {
+                return new ServiceCollection()
+                    
+                    .AddFluentMigratorCore()
+                    .ConfigureRunner(rb => rb
+                        
+                        .AddSqlServer()
+                        
+                          .WithGlobalConnectionString(conectionString)
+                        
+                        .ScanIn(typeof(Migracao).Assembly).For.Migrations())
+                    
+                    .AddLogging(lb => lb.AddFluentMigratorConsole())
+                    
+                    .BuildServiceProvider(false);
+            }
+
+            
         }
     }
-}
+
+
