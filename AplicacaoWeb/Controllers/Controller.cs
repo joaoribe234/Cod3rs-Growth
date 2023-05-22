373eb831e8d5ddd034d881028dbcb1c11f5cfd15
João Vitor Ribeiro Gonçalves <joaoribe234@gmail.com>
2023-05-22 14:04:39 -0300

    implementando projeto base SapIU5

---
+++ AplicacaoWeb/Controllers/Controller.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicacaoWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+    //    private static readonly string[] Summaries = new[]
+    //    {
+    //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    //};
+
+    //    private readonly ILogger<Controller> _logger;
+
+    //    public Controller(ILogger<Controller> logger)
+    //    {
+    //        _logger = logger;
+    //    }
+
+    //    [HttpGet(Name = "GetWeatherForecast")]
+    //    public IEnumerable<WeatherForecast> Get()
+    //    {
+    //        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //        {
+    //            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+    //            TemperatureC = Random.Shared.Next(-20, 55),
+    //            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    //        })
+    //        .ToArray();
+    //    }
+    }
+}
---
+++ AplicacaoWeb/wwwroot/Component.js
@@ -0,0 +1,22 @@
+﻿sap.ui.define(
+    [
+        "sap/ui/core/UIComponent",
+        "sap/ui/model/resource/ResourceModel",
+    ],
+    function (UIComponent, ResourceModel) {
+        "use strict";
+        return UIComponent.extend("sap.ui.petmais.Component", {
+            metadata: {
+                interfaces: ["sap.ui.core.IAsyncContentCreation"],
+                manifest: "json",
+            },
+            init: function () {
+                UIComponent.prototype.init.apply(this, arguments);
+                var i18nModel = new ResourceModel({
+                    bundleName: "sap.ui.IntetrfaceUsiario.i18n.i18n",
+                });
+                this.setModel(i18nModel, "i18n");
+            },
+        });
+    }
+);
